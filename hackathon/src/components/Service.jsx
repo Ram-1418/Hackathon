2b71eebdf5f47a7d45a1a8541cc22b55bd4be761
jayeshpandhare733 <jayeshpandhare733@gmail.com>
2024-08-31 05:57:06 +0000

    add service

---
+++ hackathon/src/components/Service.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Service() {
+  return (
+    <div>Service
+        <div className="flex justify-center items-center">
+            <ul>
+            <div class="container mx-auto p-4 h-10">
+  <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+    <img class="w-full rounded-lg " src="./Psychotherapy.jpg" alt="Kitten" /> 
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">Psychotherapy</div>
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold  py-2 px-4 rounded'>Book Appoiment</button>
+    </div> 
+  </div>
+</div>
+
+             </ul>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Service
