828982845ab53cfed1c5e1394062316f4f8c17fe
sonu-shivcharan <sonushivcharan@outlook.com>
2024-08-31 09:43:46 +0530

    initial commi

---
+++ hackathon/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ <div className='text-3xl'>
+  hello
+ </div>
+  )
+}
+
+export default App
