b63a6f04f732e09659b031437a420da7d9fc6c4c
sonu-shivcharan <sonushivcharan@outlook.com>
2024-08-31 11:38:35 +0530

    added routing to navbar

--- hackathon/src/components/Navbar.jsx
+++ hackathon/src/components/Navbar.jsx
@@ -1,71 +1,41 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
 function Navbar() {
   return (
     <nav className="bg-white text-gray-800 p-4 shadow-md h-30">
-      <div className="container mx-auto  m-auto flex justify-between items-center">
-        <Link to="/">
-          <h2 className="text-2xl font-bold">HealthFirst</h2>
-        </Link>
+    <div className="container mx-auto  m-auto flex justify-between items-center">
+      <Link to="/" className="text-2xl font-bold text-gray-800">
+       <h1> HealthFirst </h1>
+      </Link>
+    <div className='flex justify-center gap-3'>
 
-        <div className="flex justify-center gap-3">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 rounded-md font-medium 
-   ${isActive
-                ? "bg-blue-500 text-white"
-                : "text-gray-600 hover:bg-blue-100 hover:text-blue-500"
-              }
-  `
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/about"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 rounded-md font-medium 
-   ${isActive
-                ? "bg-blue-500 text-white"
-                : "text-gray-600 hover:bg-blue-100 hover:text-blue-500"
-              }
-  `
-            }
-          >
-            About
-          </NavLink>
-          <NavLink
-            to="/services"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 rounded-md font-medium 
-   ${isActive
-                ? "bg-blue-500 text-white"
-                : "text-gray-600 hover:bg-blue-100 hover:text-blue-500"
-              }
-  `
-            }
-          >
-            Services
-          </NavLink>
-
-          <NavLink
-            to="/contact"
-            className={({ isActive }) =>
-              `block py-2 pr-4 pl-3 duration-200 rounded-md font-medium 
-     ${isActive
-                ? "bg-blue-500 text-white"
-                : "text-gray-600 hover:bg-blue-100 hover:text-blue-500"
-              }
+    <NavLink
+  to="/"
+  className={({ isActive }) =>
+    `block py-2 pr-4 pl-3 duration-200 rounded-md font-medium 
+     ${isActive ? 'bg-blue-500 text-white' : 'text-gray-600 hover:bg-blue-100 hover:text-blue-500'}
     `
-            }
-          >
-            Contact
-          </NavLink>
-        </div>
-      </div>
-    </nav>
-  );
+  } 
+>
+  Home
+</NavLink>
+
+    <NavLink
+  to="/contact"
+  className={({ isActive }) =>
+    `block py-2 pr-4 pl-3 duration-200 rounded-md font-medium 
+     ${isActive ? 'bg-blue-500 text-white' : 'text-gray-600 hover:bg-blue-100 hover:text-blue-500'}
+    `
+  } 
+>
+  Contact
+</NavLink>
+
+    </div>
+    </div>
+  </nav>
+
+  )
 }
 
-export default Navbar;
+export default Navbar
