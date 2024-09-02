6edddbae6420fdf0c8c534fe67baa82fd46e14cb
23521208.acs <23521208.acs@dypvp.edu.in>
2024-09-02 06:14:59 +0000

    fixed layout for dashboard and login

--- hackathon/src/Layout.jsx
+++ hackathon/src/Layout.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import Footer from './components/Footer'
-import { Outlet, useLocation } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Dashboard from './components/Dashboard';
-import Login from './components/Login';
+import React from "react";
+import Footer from "./components/Footer";
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 
 function Layout() {
   const location = useLocation();
-  const isDashboard = location.pathname === '/dashboard'; // Check if the current route is /dashboard
-  const isLogin = location.pathname.includes("/login")
-  return (
-    <>
-   {!(isDashboard || isLogin) && <Navbar />} {/* Render Navbar only if not on Dashboard */}
-      {!(isDashboard || isLogin) && <Outlet />} {/* Render Outlet only if not on Dashboard */}
-      {isDashboard && <Dashboard />} 
-      {isLogin && <Login />} 
-      {!(isDashboard || isLogin) && <Footer />} 
-    </>
-  )
+  const isDashboard = location.pathname.includes("/dashboard"); // Check if the current route is /dashboard
+  const isLogin = location.pathname.includes("/login");
+  if (isLogin || isDashboard) {
+    return <>{isLogin ? <Login /> : <Dashboard />}</>;
+  } else {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  }
 }
 
-export default Layout
+export default Layout;
