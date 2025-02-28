22181775dc14b2ccc731afca6aba8d089d1e149b
Ram <rameshwarpatil9689@gmail.com>
2025-02-28 03:44:44 +0000

    add instruction

--- hackathon/src/components/Home.jsx
+++ hackathon/src/components/Home.jsx
@@ -208,7 +208,7 @@ function Home() {
             <iframe
               width="100%"
               height="315"
-              src="https://www.youtube.com/embed/nzDljDaQ_v8"
+              src="https://www.youtube.com/embed/nzDljDaQ_v8?autoplay=1" 
               title="YouTube video"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
