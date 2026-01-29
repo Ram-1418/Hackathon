efa8ccdae0b0dd22a388fa71cb0a1538eef34a20
Sonu Shivcharan <sonushivcharan@outlook.com>
2026-01-29 16:15:26 +0530

    Update generative model to gemini-2.5-flash

--- hackathon/src/chatbot.js
+++ hackathon/src/chatbot.js
@@ -2,5 +2,5 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import instruction from "./components/Instruction";
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEN_AI_API_KEY);
 
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" , systemInstruction:instruction });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" , systemInstruction:instruction });
 export default model;
