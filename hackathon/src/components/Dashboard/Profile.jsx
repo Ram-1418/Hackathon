0e32a495320c6c988c96587d938b6fb8f0730e9a
Ram <rameshwarpatil9689@gmail.com>
2026-02-22 11:25:24 +0000

    Refactor Profile component to improve email input handling and layout

--- hackathon/src/components/Dashboard/Profile.jsx
+++ hackathon/src/components/Dashboard/Profile.jsx
@@ -1,70 +1,70 @@
-import React, { useEffect, useState } from "react";
-function Profile({ userData, isDoctor }) {
-  const [userEmail, setUserEmail] = useState("");
-  useEffect(()=>{
-    setUserEmail(userData?.email)
-  },[userData])
-  return (
-    <div>
-      <h2 className="text-2xl font-semibold mb-4">Profile</h2>
+  import React, { useEffect, useState } from "react";
+  function Profile({ userData, isDoctor }) {
+    const [userEmail, setUserEmail] = useState("");
+    useEffect(()=>{
+      setUserEmail(userData?.email)
+    },[userData])
+    return (
+      <div>
+        <h2 className="text-2xl font-semibold mb-4">Profile</h2>
 
-      <div className="bg-white rounded-lg shadow-md p-6">
-        <div className="flex items-center">
-          <img
-            src={
-              userData?.photoURL ??
-              "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-            }
-            alt="Profile"
-            className="w-16 h-16 rounded-full mr-4"
-          />
-        
-          <div>
-            <h3 className="text-xl font-semibold text-gray-800">
-              {isDoctor==true?"Dr. ":" "}
-              {userData?.displayName}
-            </h3>
-          </div>
-        </div>
-        <div className="grid md:grid-cols-2 px-10 w-full gap-8">
-          <div className="mb-4 ">
-            <label
-              htmlFor="email"
-              className="block text-gray-700 font-semibold"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={userEmail}
-              name="email"
-              className="w-full mt-2 p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none"
-              disabled={true}
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <div className="flex items-center">
+            <img
+              src={
+                userData?.photoURL ??
+                "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+              }
+              alt="Profile"
+              className="w-16 h-16 rounded-full mr-4"
             />
+          
+            <div>
+              <h3 className="text-xl font-semibold text-gray-800">
+                {isDoctor==true?"Dr. ":" "}
+                {userData?.displayName}
+              </h3>
+            </div>
           </div>
-          {userData?.phone && (
-            <div className="mb-4">
+          <div className="grid md:grid-cols-2 px-10 w-full gap-8">
+            <div className="mb-4 ">
               <label
-                htmlFor="phone"
+                htmlFor="email"
                 className="block text-gray-700 font-semibold"
               >
-                Phone Number
+                Email
               </label>
               <input
-                type="tel"
-                id="phone"
-                value={userData?.phone}
-                name="phone"
+                type="email"
+                id="email"
+                value={userEmail}
+                name="email"
                 className="w-full mt-2 p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none"
-                readOnly
+                disabled={true}
               />
             </div>
-          )}
+            {userData?.phone && (
+              <div className="mb-4">
+                <label
+                  htmlFor="phone"
+                  className="block text-gray-700 font-semibold"
+                >
+                  Phone Number
+                </label>
+                <input
+                  type="tel"
+                  id="phone"
+                  value={userData?.phone}
+                  name="phone"
+                  className="w-full mt-2 p-3 border border-gray-300 rounded-lg focus:border-blue-500 focus:ring focus:ring-blue-200 focus:outline-none"
+                  readOnly
+                />
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default Profile;
+  export default Profile;
